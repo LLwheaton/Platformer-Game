c381b0ae9ca2fc2c09bda808be0fc8dc6f42fc07
Linda Wheaton <lwheaton89@gmail.com>
2019-10-06 19:21:42 +1100

    Including Javadoc for Controllable

--- src/main/java/stickman/model/Controllable.java
+++ src/main/java/stickman/model/Controllable.java
@@ -1,8 +1,30 @@
 package stickman.model;
 
 public interface Controllable {
+
+    /**
+     * Tells the hero to jump.
+     * @return True if succeeded, else false.
+     */
     boolean jump();
+
+    /**
+     * Tells the hero to start moving left.
+     * @return True if succeeded, else false
+     */
     boolean moveLeft();
+
+    /**
+     * Tells the hero to start moving right.
+     * @return True if succeeded, else false.
+     */
     boolean moveRight();
+
+    /**
+     * Tells the hero to stop moving left or right. This can only happen
+     * if they are currently moving - mid-jump is ok. This should not
+     * effect the jump itself.
+     * @return True if succeeded, else false.
+     */
     boolean stopMoving();
 }
