41b9317cf516d8808eff888d5766e064989e0385
Linda Wheaton <lwheaton89@gmail.com>
2019-10-06 19:44:06 +1100

    Tidy Slime

--- src/main/java/stickman/model/Slime.java
+++ src/main/java/stickman/model/Slime.java
@@ -4,17 +4,22 @@ public class Slime implements Entity {
     private String[] imagePath = {"slimeRa.png", "slimeRb.png"};
     private int index = 0;
     private String type;
-    private double velocity;
+
     private double XPos;
     private double YPos;
     private double height;
     private double width;
-    private Layer layer = Layer.FOREGROUND;
+    private double velocity;
     private double startXPos;
+    private Layer layer = Layer.FOREGROUND;
 
     public Slime(){
     }
 
+    /**
+     * Loops through image array at a slower rate than tick()
+     * @return The current imagePath
+     */
     @Override
     public String getImagePath() {
         if(index == 14){
@@ -61,27 +66,24 @@ public class Slime implements Entity {
         return this.layer;
     }
 
-    public void setHeight(double height){
-        this.height = height;
-    }
-
-    public void setWidth(double width){
-        this.width = width;
-    }
-
-    public void setVelocity(double velocity){
-        this.velocity = velocity;
-    }
-
+    @Override
     public void setXPos(double xpos){
         this.XPos = xpos;
     }
 
-    public void setStartXPos(double startXPos){
-        this.startXPos = startXPos;
-    }
+    @Override
     public void setYPos(double ypos){
         this.YPos = ypos;
+    }
+
+    @Override
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    @Override
+    public void setWidth(double width){
+        this.width = width;
     }
 
     @Override
@@ -95,6 +97,22 @@ public class Slime implements Entity {
     @Override
     public void handleCollision(Entity entity){
         this.XPos = startXPos;
+    }
+
+    /**
+     * Changes the velocity of the Slime
+     * @param velocity The new velocity of the Slime.
+     */
+    public void setVelocity(double velocity){
+        this.velocity = velocity;
+    }
+
+    /**
+     * Sets the start X postion of the slime.
+     * @param startXPos The start X position of the slime.
+     */
+    public void setStartXPos(double startXPos){
+        this.startXPos = startXPos;
     }
 
     @Override
