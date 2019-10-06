f2b626626878b9f6aa04348753cfc5ed38cd3dee
Linda Wheaton <lwheaton89@gmail.com>
2019-10-06 19:37:20 +1100

    Tidy Level

--- src/main/java/stickman/model/Level.java
+++ src/main/java/stickman/model/Level.java
@@ -42,27 +42,25 @@ public interface Level {
     double getHeroX();
 
     /**
-     * Tells the hero to jump.
+     * Direct pass through.
      * @return True if succeeded, else false.
      */
     boolean jump();
 
     /**
-     * Tells the hero to start moving left.
+     * Direct pass through.
      * @return True if succeeded, else false
      */
     boolean moveLeft();
 
     /**
-     * Tells the hero to start moving right.
+     * Direct pass through.
      * @return True if succeeded, else false.
      */
     boolean moveRight();
 
     /**
-     * Tells the hero to stop moving left or right. This can only happen
-     * if they are currently moving - mid-jump is ok. This should not
-     * effect the jump itself.
+     * Direct pass through.
      * @return True if succeeded, else false.
      */
     boolean stopMoving();
